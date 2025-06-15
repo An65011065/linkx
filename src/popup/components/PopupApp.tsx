7c0414fe7b01b7d93a9e8b309aa4f8bacd3f58e4
An Adhikari <anweshana@middlebury.edu>
2025-06-15 12:05:18 -0700

    font assets and popup setup

--- src/popup/components/PopupApp.tsx
+++ src/popup/components/PopupApp.tsx
@@ -1,10 +1,31 @@
 import React from "react";
+// import Header from "./Header";
+// import CategoryBreakdown from "./CategoryBreakdown";
+// import RecentSessions from "./RecentSessions";
+// import SecondaryMetrics from "./SecondaryMetrics";
+// import ActionControls from "./ActionControls";
+// import StatusBar from "./StatusBar";
 
 const PopupApp: React.FC = () => {
     return (
-        <div style={{ width: "400px", height: "600px", padding: "20px" }}>
-            <h1>LINKx Extension</h1>
-            <p>Hello from React popup!</p>
+        <div
+            style={{
+                width: "380px",
+                height: "580px",
+                padding: "16px",
+                fontFamily:
+                    '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, sans-serif',
+                backgroundColor: "white",
+                color: "#2d3436",
+                lineHeight: 1.4,
+            }}
+        >
+            {/* <Header />
+            <CategoryBreakdown />
+            <RecentSessions />
+            <SecondaryMetrics />
+            <ActionControls />
+            <StatusBar /> */}
         </div>
     );
 };
