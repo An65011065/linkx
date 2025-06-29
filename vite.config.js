0aa79ed0bfcbc368f11d3080518d9466d56587a6
An Adhikari <anweshana@middlebury.edu>
2025-06-29 04:47:45 -0700

    config files

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest.json";
+export default defineConfig(function (_a) {
+    var mode = _a.mode;
+    return ({
+        plugins: [react(), crx({ manifest: manifest })],
+        build: {
+            watch: mode === "development" ? {} : undefined,
+            rollupOptions: {
+                input: {
+                    background: "src/data/background.ts",
+                    popup: "src/popup/popup.html",
+                    graph: "src/graph/graph.html",
+                    dashboard: "src/dashboard/dashboard.html",
+                    main: "src/main/main.html",
+                },
+            },
+        },
+    });
+});
